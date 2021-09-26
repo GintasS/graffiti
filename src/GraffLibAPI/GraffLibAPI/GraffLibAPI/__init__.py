68a7572de472e862ed962a650ce935a470e63136
GintasS <svedgintas@gmail.com>
2021-09-26 22:10:50 +0300

    Made Swagger documentation to be the root page.

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/__init__.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/__init__.py
@@ -13,7 +13,7 @@ app.config['RESTPLUS_MASK_SWAGGER'] = False
 app.register_blueprint(user_endpoints)
 
 ### swagger specific ###
-SWAGGER_URL = '/swagger'
+SWAGGER_URL = ''
 API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/views.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/views.py
@@ -6,16 +6,6 @@ from datetime import datetime
 from flask import render_template
 from GraffLibAPI import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
