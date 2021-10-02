db6b8470a1304b350a7c700737f632cc363badd6
GintasS <svedgintas@gmail.com>
2021-10-02 15:28:47 +0300

    Subscribed to new controllers and their endpoints.

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/__init__.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/__init__.py
@@ -3,7 +3,12 @@ The flask application package.
 """
 
 from flask import Flask
-from GraffLibAPI.controllers.imagescontroller import blueprint_users as user_endpoints
+from GraffLibAPI.controllers.userscontroller import blueprint_users as user_endpoints
+from GraffLibAPI.controllers.adminscontroller import blueprint_admins as admin_endpoints
+from GraffLibAPI.controllers.imagescontroller import blueprint_images as image_endpoints
+from GraffLibAPI.controllers.markerscontroller import blueprint_markers as marker_endpoints
+from GraffLibAPI.controllers.citiescontroller import blueprint_cities as city_endpoints
+
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
@@ -11,6 +16,10 @@ app.config['RESTPLUS_MASK_SWAGGER'] = False
 
 # Register our created blueprints.
 app.register_blueprint(user_endpoints)
+app.register_blueprint(admin_endpoints)
+app.register_blueprint(image_endpoints)
+app.register_blueprint(marker_endpoints)
+app.register_blueprint(city_endpoints)
 
 ### swagger specific ###
 SWAGGER_URL = ''
@@ -26,8 +35,9 @@ app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
 import GraffLibAPI.views
-import GraffLibAPI.controllers.citycontroller
+import GraffLibAPI.controllers.citiescontroller
 import GraffLibAPI.controllers.imagescontroller
 import GraffLibAPI.controllers.markerscontroller
 import GraffLibAPI.controllers.userscontroller
 import GraffLibAPI.controllers.imagescontroller
+import GraffLibAPI.controllers.adminscontroller
