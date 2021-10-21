0f29a35cffddf4e627cb1e017ca7cf5689f9250d
Ivan <vasqw9866@gmail.com>
2021-10-21 03:09:45 +0300

    Auto stash before rebase of "refs/heads/user-by-id"

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/database/LocalTester.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy as sa
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
-import os
-engine = sa.create_engine("sqlite:///" + os.path.abspath("src/GraffLibAPI/GraffLibAPI/GraffLibAPI/database/main-db.db"))
-session = scoped_session(sessionmaker(bind=engine))
-
-print(list(session)
