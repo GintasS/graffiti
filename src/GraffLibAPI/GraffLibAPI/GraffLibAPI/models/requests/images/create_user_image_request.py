7ee090f7aada3cff254eafe2bb8e44088e53b0fd
GintasS <svedgintas@gmail.com>
2021-10-23 15:36:54 +0300

    Simplified request.

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/requests/images/create_user_image_request.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/requests/images/create_user_image_request.py
@@ -1,5 +1,6 @@
 # Library includes.
 from marshmallow import post_load
+from marshmallow import Schema, fields, validate, ValidationError
 
 # Project includes.
 from GraffLibAPI.models.image_models.image_model import ImageModel, ImageModelSchema
@@ -7,28 +8,12 @@ from GraffLibAPI.models.image_models.image_metadata_model import ImageMetadataMo
 from GraffLibAPI.models.image_models.image_classification_model import ImageClassificationModel, ImageClassificationModelSchema
 from GraffLibAPI.models.image_models.image_location_model import ImageLocationModel, ImageLocationModelSchema
 
-class CreateUserImageRequest(ImageModel):
-    def __init__(self, image_metadata_model, image_classification_model):
-
-        # TODO: Fix this manual mapping in this request and in ImageLocationSchema.
-        image_metadata_model_object = ImageMetadataModel(image_metadata_model["extension"], image_metadata_model["photographed_time"], image_metadata_model["upload_time"], ImageLocationModel(image_metadata_model["image_location_model"]["coordinates"]))
-        image_classification_model_object = ImageClassificationModel(image_classification_model["user_provided_name"], image_classification_model["description"], image_classification_model["graffiti_object"], image_classification_model["image_direction"])
-
-        super(CreateUserImageRequest, self).__init__("", "", image_metadata_model_object, image_classification_model_object)
+class CreateUserImageRequest():
+    def __init__(self, image_classification_model, user_id):
+        self.image_classification_model = image_classification_model
+        self.user_id = user_id
 
     def __repr__(self):
         return '<CreateUserImageRequest(name={self.user_id!r})>'.format(self=self)
 
 # TODO: Remove unused comments.
-
-
-# Note that besides the User class, 
-# we also defined a UserSchema. 
-# We will use the latter to deserialize and serialize instances of User 
-# from and to JSON objects.
-
-# **kwargs absorbs unmatched fields.
-class CreateUserImageRequestSchema(ImageModelSchema):
-    @post_load
-    def make_create_user_image_request(self, data, **kwargs):
-        return CreateUserImageRequest(**data)
