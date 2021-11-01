9a81c610359207d4e21ca08feb5e7a4e4bb9cf1d
GintasS <svedgintas@gmail.com>
2021-11-01 20:55:12 +0200

    Refactored controllers

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/controllers/cities_controller.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/controllers/cities_controller.py
@@ -1,64 +1,69 @@
-# Library imports
+# Library imports.
 import json
 from flask import Blueprint, render_template, request, jsonify
-import datetime as dt
 from sqlalchemy import update, and_, or_, not_
 from marshmallow import ValidationError
-from secrets import token_urlsafe
-import bcrypt
-import hashlib
 
-# Project imports
-from GraffLibAPI.models.enums import *
-from GraffLibAPI.models.requests.create_city_request import *
-from GraffLibAPI.models.responses.create_city_response import *
-from GraffLibAPI.models.city_model import CityModel
-from GraffLibAPI.database.entities.city_entity import *
+# Project imports.
 from GraffLibAPI.database.db_setup import session
+from GraffLibAPI.database.entities.city_entity import CityEntity, CityEntitySchema
+from GraffLibAPI.models.requests.create_city_request import CreateCityRequestSchema
 from GraffLibAPI.mappings.mappings import *
 
-# A blueprint is an object very similar to a flask application object, but instead of creating a new one, 
-# it allows the extension of the current application.
+blueprint_cities = Blueprint("api-cities", __name__, url_prefix="/v1/cities")
 
-# This might be useful if you want to create multiple versions of an API or simply 
-# divide services within the same application.
-blueprint_cities = Blueprint('api-cities', __name__, url_prefix='/v1/cities')
+@blueprint_cities.route("", methods=["GET"])
+def get_cities():
+    try:
+        cities = session.query(CityEntity).all()
 
-@blueprint_cities.route('', methods=['POST'])
+        return { "body": CityEntitySchema(many=True).dump(cities) }, 200
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    except:
+        return "Internal server error.", 500
+
+@blueprint_cities.route("", methods=["POST"])
 def create_city():
     try:
         city_request = CreateCityRequestSchema().load(request.get_json())
         found_city = session.query(CityEntity).\
             filter(
-                or_(
-                    CityEntity.city_name.like(city_request.city_name),
-                )
+                CityEntity.city_name.like(city_request.city_name),
             ).\
             first()
 
         if found_city is not None:
             return "City already exists", 409
 
-        new_city = to_city_entity(city_request)
-        session.add(new_city)
+        city_entity = to_city_entity(city_request)
+
+        session.add(city_entity)
         session.commit()
 
-        return to_create_city_response(new_city).toJSON(), 201
+        return { "body": CityEntitySchema().dump(city_entity) }, 201
     except ValidationError as err:
         return jsonify(err.messages), 400
     except:
-        return "Internal server errror.", 500
+        return "Internal server error.", 500
 
-
-@blueprint_cities.route('', methods=['GET'])
-def get_city():
+@blueprint_cities.route("/<int:city_id>/", methods=["DELETE"])
+def delete_city(city_id):
     try:
-        cities = session.query(CityEntity).all()
-        city_entity_schema = CityEntitySchema(many=True)
+        found_city = session.query(CityEntity).\
+            filter(
+                CityEntity.id == city_id
+            ).\
+            first()
+
+        if found_city is None:
+            return "City was not found.", 404
+
+        session.delete(found_city)
+        session.commit()
+
+        return "", 204
+    except ValidationError as err:
+        return jsonify(err.messages), 400
     except:
-        return "Internal server errror.", 500
-    return {
-        'message': 'This PATCH13 endpoint should update the entity',
-        'method': request.method,
-        'body': city_entity_schema.dump(cities)
-    }
+        return "Internal server error.", 500
