2c54a2d878fe62cbf92963883968583d3867925f
GintasS <svedgintas@gmail.com>
2021-11-01 20:56:00 +0200

    Added validation for response models.

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/responses/create_city_response.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/responses/create_city_response.py
@@ -1,9 +1,10 @@
 import json
 
 # Marshmallow is a popular Python package for converting complex datatypes, such as objects, to and from native Python datatypes.
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate, ValidationError
 from marshmallow import post_load
 from GraffLibAPI.models.city_model import CityModel, CityModelSchema
+from GraffLibAPI.configuration.constants import *
 
 class CreateCityResponse():
     def __init__(self, city_id, city_name):
@@ -15,6 +16,9 @@ class CreateCityResponse():
             sort_keys=True)
 
 class CreateCityResponseSchema():
+    city_id = fields.Int(required=True)
+    city_name = fields.Str(required=True, validation=validate.Length(LocationValidation.CITY_NAME_MIN_LENGTH, LocationValidation.CITY_NAME_MAX_LENGTH, error=LocationValidation.CITY_NAME_VALIDATION_MSG))
+
     @post_load
     def make_create_city_request(self, data, **kwargs):
         return CreateCityResponse(**data)
--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/responses/create_user_response.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/models/responses/create_user_response.py
@@ -1,9 +1,10 @@
 import json
 
 # Marshmallow is a popular Python package for converting complex datatypes, such as objects, to and from native Python datatypes.
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate, ValidationError
 from marshmallow import post_load
 from GraffLibAPI.models.user_model import UserModel, UserModelSchema
+from GraffLibAPI.configuration.constants import *
 
 class CreateUserResponse():
     def __init__(self, user_id, user_name, email, created_at):
@@ -20,7 +21,8 @@ class CreateUserResponse():
 # **kwargs absorbs unmatched fields.
 
 class CreateUserResponseSchema(Schema):
-    user_id = fields.Int()
-    user_name = fields.Str()
-    email = fields.Email()
-    created_at = fields.Date()
+    user_id = fields.Int(required=True)
+    first_name = fields.Str(required=True, validate=validate.Length(max=UserValidation.FISRT_NAME_MAX_LENGTH, error=UserValidation.FIRST_NAME_VALIDATION_MSG))
+    last_name = fields.Str(required=True, validate=validate.Length(max=UserValidation.LAST_NAME_MAX_LENGTH, error=UserValidation.LAST_NAME_VALIDATION_MSG))
+    email = fields.Email(required=True, validate=validate.Length(UserValidation.EMAIL_MIN_LENGTH, UserValidation.EMAIL_MAX_LENGTH, error=UserValidation.EMAIL_VALIDATION_MSG))
+    created_at = fields.DateTime(required=True, format=MiscValidation.DATE_FORMAT)
