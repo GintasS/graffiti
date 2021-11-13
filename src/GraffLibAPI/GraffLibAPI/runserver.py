8dc6ce1021c5ab2fa4f2400a92dd6da19afe16aa
GintasS <svedgintas@gmail.com>
2021-11-13 20:02:39 +0200

    Simplified main project script to use single port.

--- src/GraffLibAPI/GraffLibAPI/runserver.py
+++ src/GraffLibAPI/GraffLibAPI/runserver.py
@@ -6,9 +6,4 @@ from os import environ
 from GraffLibAPI import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
-    except ValueError:
-        PORT = 8000
-    app.run(HOST, PORT)
+   app.run(host='0.0.0.0', port=8000)
