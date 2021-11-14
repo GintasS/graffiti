c1a82f34bf03e44e990203d513f6a7ddf26c7c13
GintasS <svedgintas@gmail.com>
2021-11-14 17:49:50 +0200

    Added TODO comment.

--- src/GraffLibAPI/GraffLibAPI/runserver.py
+++ src/GraffLibAPI/GraffLibAPI/runserver.py
@@ -5,5 +5,7 @@ This script runs the GraffLibAPI application using a development server.
 from os import environ
 from GraffLibAPI import app
 
+# TODO: [PRODUCTION] Do not use DEV build for PROD.
+
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8000)
