e10480cec90e139c6bb32dfc3bf9932cff90d8b2
GintasS <svedgintas@gmail.com>
2021-11-18 11:40:58 +0200

    Added TODO comment.

--- src/GraffLibAPI/GraffLibAPI/runserver.py
+++ src/GraffLibAPI/GraffLibAPI/runserver.py
@@ -6,6 +6,7 @@ from os import environ
 from GraffLibAPI import app
 
 # TODO: [PRODUCTION] Do not use DEV build for PROD.
+# TODO: [PYTHON] Update project to use newest python version and newest packages.
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8000)
