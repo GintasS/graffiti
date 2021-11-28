f995a3e6979236dd3584d37d28dd8fbc41f754a9
GintasS <svedgintas@gmail.com>
2021-11-28 18:35:44 +0200

    Started using PROD server instead of development one on PROD.

--- src/GraffLibAPI/GraffLibAPI/runserver.py
+++ src/GraffLibAPI/GraffLibAPI/runserver.py
@@ -2,6 +2,7 @@
 This script runs the GraffLibAPI application using a development server.
 """
 
+from gevent.pywsgi import WSGIServer
 from os import environ
 from GraffLibAPI import app
 
@@ -9,5 +10,5 @@ from GraffLibAPI import app
 # TODO: [PYTHON] Update project to use newest python version and newest packages.
 # TODO: [EMAIL] After user signs up, we need a confirmation email logic.
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8000)
+http_server = WSGIServer(('0.0.0.0', 8000), app)
+http_server.serve_forever()
