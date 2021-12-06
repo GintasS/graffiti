170602826f32acb6133467da863eaf2170bccfc4
GintasS <svedgintas@gmail.com>
2021-12-06 19:16:54 +0200

    Added logging.

--- src/GraffLibAPI/GraffLibAPI/GraffLibAPI/utils/file_helper.py
+++ src/GraffLibAPI/GraffLibAPI/GraffLibAPI/utils/file_helper.py
@@ -2,10 +2,15 @@ from pathlib import Path
 import os
 import os.path as pathh
 from secrets import token_urlsafe
+import logging
+logger = logging.getLogger()
+
 
 def save_file(file_bytes : bytes, directory : str, unique_image_name : str, file_extension : str):
     file_name_with_extension = unique_image_name + "." + file_extension
     new_file_path = os.path.join(directory, file_name_with_extension)
+    logger.warning("SAVING IMAGE TO PATH: " + new_file_path)
+
 
     new_file = open(new_file_path, 'wb')
     new_file.write(file_bytes)
@@ -13,7 +18,17 @@ def save_file(file_bytes : bytes, directory : str, unique_image_name : str, file
 
 def get_current_directory(marker_id : str) -> str:
     # "" string creates a new directory!
-    return os.path.join(str(Path.cwd()), "GraffLibAPI", "static", "markers", marker_id)
+    full_directory_path = os.path.join(str(Path.cwd()), "GraffLibAPI", "static", "markers", marker_id)
+    
+    logger.warning("DIRECTORY PATH: " + full_directory_path)
+    return full_directory_path
+
+def get_image_full_path(directory : str, image_unique_name : str, file_extension : str):
+    file_name_with_extension = image_unique_name + "." + file_extension
+    image_full_path = os.path.join(directory, file_name_with_extension)
+    
+    logger.warning("IMAGE FULL PATH: " + image_full_path)
+    return image_full_path
 
 def create_user_image_directory_name():
     return token_urlsafe(16)
